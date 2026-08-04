c0bcc69cb2fec4c2ec6d7c284e6aa6436d3e0a87
WR Allbritain <wes.allbritain@gmail.com>
2026-08-03 19:25:33 -0500

    06.Building... use utilities.h

--- 03.FirstSteps/06.BuildingWithCMakePresets/02.Project_template_headers/main.cpp
+++ 03.FirstSteps/06.BuildingWithCMakePresets/02.Project_template_headers/main.cpp
@@ -1,12 +1,26 @@
-/*
-    . Project description
-        . Topic #1
-        . Topic #2
-*/
+#include <fmt/core.h>
+#include <zlib.h>
+#include <string>
+#include <string_view>
 
 
 #include "utilities.h"
 
 int main() {
+
+    double a = 26.153;
+    double b = 46.782;
+    std::string c = "Hello, and greetings!";
+    std::string_view d = "Goodbye!!!";
+
+    fmt::print("{}\n", c);
+
+    fmt::print("fmt  version : {}\n"
+               "ZLIB version : {}\n",
+                FMT_VERSION, ZLIB_VERSION);
+
+    fmt::print("{} + {} = {}\n", a, b, add(a,b));
+
+    fmt::print("{}\n", d);
     
 }
