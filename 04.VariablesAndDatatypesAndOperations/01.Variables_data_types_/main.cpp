12459aacf4585882554d3b7cf4013df45966fa4b
WR Allbritain <wes.allbritain@gmail.com>
2026-08-04 16:06:52 -0500

    add - 04.VariablesAndDatatypesAndOperations add - 01.Variables_data_types_

---
+++ 04.VariablesAndDatatypesAndOperations/01.Variables_data_types_/main.cpp
@@ -0,0 +1,17 @@
+/*
+    . Exploring variables and data types
+*/
+
+#include <fmt/format.h>
+
+// One line comment
+import utilities;
+
+int main(){
+
+    //number_systems();
+
+    //floating_point_types();
+
+    numeric_limits();
+}
